a708b0726679b37892b32263e5f3176133b348c2
IanDinizFK <iandiniz653@gmail.com>
2023-02-08 13:47:37 -0300

    Projeto NLW Setup

---
+++ web/src/components/ProgressBar.tsx
@@ -0,0 +1,15 @@
+interface ProgressBarProps {
+    progress: number
+}
+
+export function ProgressBar(props: ProgressBarProps){
+
+    return(
+        <div className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
+                <div role="progressbar" arial-label="Progresso de hábitos completados nesse dia" 
+                className="h-3 rounded-xl bg-violet-600 transition-all" aria-valuenow={props.progress} style={{width: `${props.progress}%`}}>
+                </div>
+        </div>
+    )
+
+}
